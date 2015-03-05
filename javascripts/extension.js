91e846f75b1a1dd96835818cad039bfd654f0407
FloensBot <FloensBot@users.noreply.github.com>
2015-03-05 05:01:18 +0100

    M  javascripts/extension.js

--- javascripts/extension.js
+++ javascripts/extension.js
@@ -315,9 +315,9 @@ Parser.buildHTMLFromJSON = function(data, board, standalone, fromQuote) {
         case 'admin_highlight':
             highlight = ' highlightPost';
         case 'admin':
-            capcodeStart = ' <strong class="capcode hand id_admin"' + 'title="Highlight posts by the Administrator">## Admin</strong>';
+            capcodeStart = ' <strong class="capcode hand id_admin" ' + 'title="Highlight posts by Administrators">## Admin</strong>';
             capcodeClass = ' capcodeAdmin';
-            capcode = ' <img src="' + Parser.icons.admin + '" ' + 'alt="This user is the 4chan Administrator." ' + 'title="This user is the 4chan Administrator." class="identityIcon">';
+            capcode = ' <img src="' + Parser.icons.admin + '" ' + 'alt="This user is a 4chan Administrator." ' + 'title="This user is a 4chan Administrator." class="identityIcon">';
             break;
         case 'mod':
             capcodeStart = ' <strong class="capcode hand id_mod" ' + 'title="Highlight posts by Moderators">## Mod</strong>';
@@ -333,6 +333,11 @@ Parser.buildHTMLFromJSON = function(data, board, standalone, fromQuote) {
             capcodeStart = ' <strong class="capcode hand id_manager" ' + 'title="Highlight posts by Managers">## Manager</strong>';
             capcodeClass = ' capcodeManager';
             capcode = ' <img src="' + Parser.icons.manager + '" ' + 'alt="This user is a 4chan Manager." ' + 'title="This user is a 4chan Manager." class="identityIcon">';
+            break;
+        case 'admin_emeritus':
+            capcodeStart = ' <strong class="capcode hand id_admin" ' + 'title="Highlight posts by the Administrator Emeritus">## Admin Emeritus</strong>';
+            capcodeClass = ' capcodeAdmin';
+            capcode = ' <img src="' + Parser.icons.admin + '" ' + 'alt="This user is 4chan\'s founding Administrator." ' + 'title="This user is 4chan\'s founding Administrator." class="identityIcon">';
             break;
     }
     if (data.email) {
@@ -3002,7 +3007,7 @@ ThreadWatcher.canAutoRefresh = function() {
     if (time = localStorage.getItem('4chan-tw-timestamp')) {
         return Date.now() - (+time) >= 60000;
     }
-    return false;
+    return true;
 };
 ThreadWatcher.setRefreshTimestamp = function() {
     localStorage.setItem('4chan-tw-timestamp', Date.now());
@@ -6227,7 +6232,7 @@ Main.onclick = function(e) {
             if (ImageExpansion.toggle(t)) {
                 e.preventDefault();
             }
-        } else if (Config.inlineQuotes && e.which == 1 && $.hasClass(t, 'quotelink')) {
+        } else if (Config.inlineQuotes && e.which == 1 && $.hasClass(t, 'quotelink') && Main.page !== 'archive') {
             if (!e.shiftKey) {
                 QuoteInline.toggle(t, e);
             } else {
