1d3b32a8f400a9dee234a23b3b7b4d4d33c12a14
Floens <Floens@gmail.com>
2015-06-19 11:01:40 +0200

    Add janichan.css
    Closes #1

--- track.js
+++ track.js
@@ -77,6 +77,7 @@ var loadNewCss = function(version) {
     loadNewCssAndBeautify('https://s.4cdn.org/css/tomorrow.' + version + '.css', 'tomorrow');
     loadNewCssAndBeautify('https://s.4cdn.org/css/yotsubluemobile.' + version + '.css', 'yotsubluemobile');
     loadNewCssAndBeautify('https://s.4cdn.org/css/yui.1.css', 'yui');
+    loadNewCssAndBeautify('https://s.4cdn.org/css/janichan.css', 'janichan');
 }
 
 var onHtmlResponse = function(response) {
