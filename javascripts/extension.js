e50821b46d05e13a8ec681b4442a14f333933386
FloensBot <FloensBot@users.noreply.github.com>
2016-12-28 09:00:06 +0100

    M  javascripts/extension.js

--- javascripts/extension.js
+++ javascripts/extension.js
@@ -1056,7 +1056,7 @@ Parser.parseTrackedReplies = function(post) {
   
   for (i = 0; link = quotelinks[i]; ++i) {
     if (Parser.trackedReplies[link.textContent]) {
-      link.textContent += ' (You)';
+      link.className += ' ql-tracked';
       Parser.hasYouMarkers = true;
     }
   }
@@ -10014,6 +10014,7 @@ div.collapseWebm { text-align: center; margin-top: 10px; }\
 #yt-preview { position: absolute; }\
 #yt-preview img { display: block; }\
 .autohide-nav { transition: top 0.2s ease-in-out }\
+.ql-tracked { text-decoration: none; border-bottom: 1px dotted; }\
 #feedback {\
   position: fixed;\
   top: 10px;\
