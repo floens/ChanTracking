c2bf7155081375ceec962af2dec8cc3217959289
Florens <Floens@gmail.com>
2017-10-30 22:41:41 +0100

    spooky2017

--- track.js
+++ track.js
@@ -191,6 +191,7 @@ var load = function() {
     loadCssAndBeautify('https://s.4cdn.org/css/md2016.' + now + '.css', 'md2016');
     loadCssAndBeautify('https://s.4cdn.org/css/error.' + now + '.css', 'error');
     loadCssAndBeautify('https://s.4cdn.org/css/bans.' + now + '.css', 'bans');
+    loadCssAndBeautify('https://s.4cdn.org/css/spooky2017.' + now + '.css', 'spooky2017');
 }
 
 process.on('uncaughtException', function(e) {
